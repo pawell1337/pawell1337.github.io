64f5bfb4a49e89418d7cf5ede6c2a1f15ebc571c
pawell1337 <pawelstudia55@gmail.com>
2025-12-01 12:41:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
   const countElement = document.querySelector('.testimonial-count strong');
   let currentCount = 0;
-  const targetCount = 100000;
+  const targetCount = 10000;
   
   function animateCount() {
     if (currentCount < targetCount) {
       currentCount += 100;
       countElement.textContent = currentCount.toLocaleString();
-      setTimeout(animateCount, 20);
+      setTimeout(animateCount, 10);
     }
   }
   
